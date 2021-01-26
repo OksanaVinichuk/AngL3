0c4c688ece593583bf3f559ba0332fecdb961143
OksanaVinichuk <OViniTheChuk@gmail.com>
2021-01-26 20:31:56 +0200

    practice 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import {HttpClient, HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    UsersComponent,
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
